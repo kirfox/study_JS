2992f64abe57417961d8764a0941c068fd806faa
kirfox <muxin.kirill2011@yandex.ru>
2020-08-06 12:24:28 +0300

    Выполнил 1 дз

---
+++ lesson01/script.js
@@ -0,0 +1,16 @@
+//declare variables
+let money, 
+    income, 
+    addExpenses, 
+    deposit, 
+    mission, 
+    period;
+
+//output "Hello World!" in modal window
+    alert("Hello World!");
+
+//output "Hello World Again!" in console
+    console.log("Hello World Again!");
+    
+
+    
