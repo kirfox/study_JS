f911a829cfcfd83b70ab3d221d18b40c8fc06214
kirfox <muxin.kirill2011@yandex.ru>
2020-08-07 12:57:18 +0300

    Сделал 2 дз

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,16 +1,18 @@
 //declare variables
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money = 50000, 
+    income = 20000, 
+    addExpenses ='Коммуналка, Интернет, Продукты', 
+    deposit = false, 
+    mission = 1000000, 
+    period = 6,
+    budgetDay = money/30;
 
-//output "Hello World!" in modal window
-    alert("Hello World!");
-
-//output "Hello World Again!" in console
-    console.log("Hello World Again!");
-    
-
-    
+//output in console
+    console.log(typeof money,
+                typeof income,
+                typeof deposit);
+    console.log(addExpenses.length);
+    console.log('Период равен ' + period + ' месяцев');
+    console.log('Цель заработать ' + mission + ' рублей');
+    console.log(addExpenses.toLowerCase().split(', '));
+    console.log(budgetDay);
