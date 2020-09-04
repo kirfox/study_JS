5fcdffca6da221e71b180baf1818d435c133bbdb
kirill <muxin.kirill2011@yande.ru>
2020-09-04 13:32:38 +0300

    problems with get key

---
+++ lesson22/js/script.js
@@ -0,0 +1,104 @@
+'use strict';
+
+class Todo {
+    constructor(form, input, todoList, todoCompleted, todoRemove) {
+        this.form  = document.querySelector(form);
+        this.input  = document.querySelector(input);
+        this.todoList  = document.querySelector(todoList);
+        this.todoCompleted  = document.querySelector(todoCompleted);
+        this.todoRemove  = document.querySelectorAll(todoRemove);
+        this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+     
+    }
+
+    addToStorage(){
+        localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+    }
+
+    render(){
+        this.todoList.textContent = '';
+        this.todoCompleted.textContent = '';
+        this.todoData.forEach(this.createItem, this);
+        this.addToStorage();
+    }
+
+    createItem(todo){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.key = todo.key;
+        li.insertAdjacentHTML('beforeend', `
+        <span class="text-todo">${todo.value}</span>
+        <div class="todo-buttons">
+            <button class="todo-remove"></button>
+            <button class="todo-complete"></button>
+        </div>
+        `);
+    
+        if (todo.completed) {
+            this.todoCompleted.append(li);
+        } else {
+            this.todoList.append(li);
+        }
+       
+    }
+
+
+    addTodo(e){
+        e.preventDefault();
+        if (this.input.value.trim()) {
+            const newTodo = {
+                value: this.input.value,
+                completed: false,
+                key: this.generateKey(),
+            };
+            this.todoData.set(newTodo.key, newTodo);
+            this.render();
+        }
+       
+    }
+
+    generateKey(){
+        return Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15);
+    }
+
+    deleteItem(){
+        console.log(event.target);
+        
+        
+    }
+
+    completedItem(){
+        console.log(2);
+    }
+
+    handler(){
+      
+        this.todoList.addEventListener('click', () =>{
+            let target = event.target;
+             
+            if (target.closest('.todo-item')) {
+                this.deleteItem();
+            } 
+            if (target.classList.contains('todo-complete')) {
+                this.completedItem();
+            } 
+            
+        });
+      
+    
+    }
+
+    init() {
+        this.form.addEventListener('submit', this.addTodo.bind(this));
+        
+        this.render();
+
+        this.handler();
+        
+       
+    }
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed', '.todo-remove');
+
+todo.init();
